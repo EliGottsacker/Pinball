83063fa34da2f85157f511c4c314269eee3ef50d
eli gottsaker <eligottsacker@gmail.com>
2018-05-11 07:30:56 -0700

    changed my code

--- ElisCode/maincode_copy/maincode_copy.ino
+++ ElisCode/maincode_copy/maincode_copy.ino
@@ -9,7 +9,11 @@
 * [needs work]display intergation
 * [ASAP]test code
 *
-*
+*this is how to use a larger int it is lite and has a range between 0 to 65,535((2^16) - 1 ) but must not be negitve 
+* https://www.arduino.cc/reference/en/language/variables/data-types/unsignedint/
+* 
+* and this is for longs they have a range between -2,147,483,648 to 2,147,483,647. more than we will ever need
+* https://www.arduino.cc/reference/en/language/variables/data-types/long/
 */
 
 
@@ -61,7 +65,7 @@ const int newBalls = 5; //the number of new ball per pament
 const int minCoinsRequerd = 2; // minnum coins requard to get balls
 
 //points
-const int pointsForPopBumper = 1;
+const int pointsForPopBumper = 1;//ardrino max int is 32,767 so score has 0 add on end 
 const int pointsForSlingShot = 1;
 const int pointsForRollOver = 1;
 
@@ -154,12 +158,7 @@ void GameControl(){
     int ballState = digitalRead(ballDeathSwith);
     bool ballCounted;
 
-    message = "000000";
-
-
-    //Serial.read()
-
-    
+    message = "888888";
 
     if (coinDetected == HIGH) {
         if(coinCounted == false){
@@ -188,12 +187,20 @@ void GameControl(){
     } else {
         ballCounted = false;
     }
+    if (score > 32757){
+      score = 0
+      Serial.print("Max Score Reached. Wow.");
+      
+    }
 
     //this checks if requarments to start the game has been reheched and
     if (gameState == true || true){
         if (score > freeBallPointsTracker){
             AddBalls(freeBalls);
             freeBallPointsTracker = score + freeBallPoints;
+            if (freeBallPointsTracker > 16000){
+              freeBallPointsTracker = freeBallPoints;
+            }
         }
         DisplayInt(score);
     } else {
@@ -220,96 +227,96 @@ void AddScore(int _score){
 }
 
 void DisplayInt(int score) {
-int setLength = 1;
-if (score > 9) {
-setLength = 2;
-}
-if (score > 99) {
-setLength = 3;
-}
-if (score > 999) {
-setLength = 4;
-}
-if (score > 9999) {
-setLength = 5;
-}
-if (score > 99999) {
-setLength = 6;
-}
-
-String setstring;
-int getChar;
-String comb;
-String comb2;
-
-for (int i = 0; i < setLength; i++) {
-getChar = 0;
-if (i == 0) {
-getChar = (score / 1U) % 10;
-}
-if (i == 1) {
-getChar = (score / 10U) % 10;
-}
-if (i == 2) {
-getChar = (score / 100U) % 10;
-}
-if (i == 3) {
-getChar = (score / 1000U) % 10;
-}
-if (i == 4) {
-getChar = (score / 10000U) % 10;
-}
-if (i == 5) {
-getChar = (score / 100000U) % 10;
-}
-
-comb2 = setstring;
-
-if (getChar == 0) {
-comb = "0";
-}
-if (getChar == 1) {
-comb = "1";
-}
-if (getChar == 2) {
-comb = "2";
-}
-if (getChar == 3) {
-comb = "3";
-}
-if (getChar == 4) {
-comb = "4";
-}
-if (getChar == 5) {
-comb = "5";
-}
-if (getChar == 6) {
-comb = "6";
-}
-if (getChar == 7) {
-comb = "7";
-}
-if (getChar == 8) {
-comb = "8";
-}
-if (getChar == 9) {
-comb = "9";
-}
-if (getChar != 0 && getChar != 1 && getChar != 2 && getChar != 3 && getChar != 4 && getChar != 5 && getChar != 6 && getChar != 7 && getChar != 8 && getChar != 9) {
-Serial.println("SOMETHING HAS GONE HORRIBLY WRONG IF THIS PRINTS");
-}
-setstring = comb + comb2;
-//setstring = String(getChar) + setstring;
-}
-
-//Serial.print(setLength);
-
-for (int i = 0; i < 5-setLength; i++) {
-setstring = "a"+setstring;
-}
-
-message = setstring+"0";
-//Serial.print(message);
+  int setLength = 1;
+  if (score > 9) {
+    setLength = 2;
+  }
+  if (score > 99) {
+    setLength = 3;
+  }
+  if (score > 999) {
+    setLength = 4;
+  }
+  if (score > 9999) {
+    setLength = 5;
+  }
+  if (score > 99999) {
+    setLength = 6;
+  }
+  
+  String setstring;
+  int getChar;
+  String comb;
+  String comb2;
+  
+  for (int i = 0; i < setLength; i++) {
+    getChar = 0;
+    if (i == 0) {
+      getChar = (score / 1U) % 10;
+    }
+    if (i == 1) {
+      getChar = (score / 10U) % 10;
+    }
+    if (i == 2) {
+      getChar = (score / 100U) % 10;
+    }
+    if (i == 3) {
+      getChar = (score / 1000U) % 10;
+    }
+    if (i == 4) {
+      getChar = (score / 10000U) % 10;
+    }
+    if (i == 5) {
+      getChar = (score / 100000U) % 10;
+    }
+    
+    comb2 = setstring;
+    
+    if (getChar == 0) {
+      comb = "0";
+    }
+    if (getChar == 1) {
+      comb = "1";
+    }
+    if (getChar == 2) {
+      comb = "2";
+    }
+    if (getChar == 3) {
+      comb = "3";
+    }
+    if (getChar == 4) {
+      comb = "4";
+    }
+    if (getChar == 5) {
+      comb = "5";
+    }
+    if (getChar == 6) {
+      comb = "6";
+    }
+    if (getChar == 7) {
+      comb = "7";
+    }
+    if (getChar == 8) {
+      comb = "8";
+    }
+    if (getChar == 9) {
+      comb = "9";
+    }
+    if (getChar != 0 && getChar != 1 && getChar != 2 && getChar != 3 && getChar != 4 && getChar != 5 && getChar != 6 && getChar != 7 && getChar != 8 && getChar != 9) {
+      Serial.println("SOMETHING HAS GONE HORRIBLY WRONG IF THIS PRINTS");
+    }
+    setstring = comb + comb2;
+    //setstring = String(getChar) + setstring;
+  }
+  
+  //Serial.print(setLength);
+  
+  for (int i = 0; i < 5-setLength; i++) {
+    setstring = "a"+setstring;
+  }
+  
+  message = setstring+"0";
+  //Serial.print(message);
 }
 
 void setMessage(String mess) {
@@ -355,42 +362,6 @@ void StartGame(){
     BallGateControl(true);
     //AddMessage();
 }
-
-
-/*
-void DisplayInt(int _int){//NOT DONE, NOT TESTED
-    //String outputSting;
-    
-    
-    char outputChar[messageArraySize];
-    int countOfNeededZeros;
-    int lengthOutputChar = sizeof(outputChar);
-    char neededZeros[]];
-    
-
-    if (true){
-        for(int i = 0; i < countOfNeededZeros ; i++){
-            neededZeros[i] = '0'; 
-        }
-        outputChar[messageArraySize] =  neededZeros + char(_int) ;
-    }
-    //outputSting = String();
-    message = outputChar
-    
-}*/
-
-
-/*
-void DisplayManger(String _str){ //NOT DONE. do not use untill test rest of code. This will make it so display it show proper timing
-    char oldMessage;
-    int oldScore;
-    if (message != oldMessage || score != oldScore){
-
-    }
-}
-*/
-
-
 
 bool RollOverSwichLogic(int _inputPin){// NOT TESTED will take in a pin and making into a bool roll over switch
     int rollOverSwich = digitalRead(_inputPin);
@@ -442,8 +413,8 @@ void LedGridLightUP(){
 
 
 void ElectronicsLoop(){
-    //SlingshotControl(slingshotOne, slingshotOneSwitch, pointsForSlingShot);
-    //SlingshotControl(slingshotTwo, slingshotTwoSwitch, pointsForSlingShot);
+    SlingshotControl(slingshotOne, slingshotOneSwitch, pointsForSlingShot);
+    SlingshotControl(slingshotTwo, slingshotTwoSwitch, pointsForSlingShot);
     PopBumperControl(popBumperOne, popBumperOneSwitch, pointsForPopBumper);
     PopBumperControl(popBumperTwo, popBumperTwoSwitch, pointsForPopBumper);
     PopBumperControl(popBumperThree, popBumperThreeSwitch, pointsForPopBumper);
@@ -533,7 +504,7 @@ void FlipperControl(int _flipperPowerCoil, int _flipperHoldCoil, int _flipperPow
         digitalWrite(_flipperHoldCoil, LOW);
     }
 }
-/*
+
 void SlingshotControl(int _slingshot, int _slingshotSwitch, int _points) {//NOT DONE
 
     int buttonState = 1; // variable for reading the pushbutton status
@@ -552,6 +523,6 @@ void SlingshotControl(int _slingshot, int _slingshotSwitch, int _points) {//NOT 
         // turn LED off:
         digitalWrite(_slingshot, LOW);
     }
-}*/
+}
 
 
