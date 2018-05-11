ec53ebdab1af9d902faf9a850f8c531a1420c314
eliG2001 <eligottsacker@gmail.com>
2018-05-11 11:46:05 -0700

    commit

---
+++ trash/5-10-Working/Pinball-master/Code/other/working_for_maze/working_for_maze.ino
@@ -0,0 +1,258 @@
+/* This is the code for the main microcontroler.
+*
+*
+*This control jobs are:
+* [x]coin counter
+* [ ]fliper control
+* [x]com with screen ardino
+* [ ]games played
+* [needs work]display intergation
+* [ASAP]test code
+*
+*
+*/
+
+
+//*****************************************************************************************************************
+// Decare these varible to the corisopning Aurdino pins and wanted vaules for tuning game play exsperence
+
+
+// constants won't change. They're used here to set pin numbers:
+const int popBumperOneSwitch = A0; // the number of the switch pin
+const int popBumperOne = 5; // the number of the popBumper pin
+//const int popBumperOneLED = 14; // the number of the popBumperLED pin
+
+const int popBumperTwoSwitch = A1; // the number of the switch pin
+const int popBumperTwo = 6; // the number of the popBumper pin
+//const int popBumperTwoLED = 15; // the number of the popBumperLED pin
+
+
+const int popBumperThreeSwitch = A2; // the number of the switch pin
+const int popBumperThree = 7; // the number of the popBumper pin
+//const int popBumperThreeLED = 16; // the number of the popBumperLED pin
+
+const int flipperRightHoldSwitch = A15; // the number of the switch pin
+const int flipperRightHoldCoil = 8; //
+const int flipperRightPowerSwitch = A4; //
+const int flipperRightPowerCoil = 11; // the number of the flipper pin
+
+const int flipperLeftHoldSwitch = A8; // the number of the switch pin
+const int flipperLeftHoldCoil = 10; //
+
+const int flipperLeftPowerSwitch = A11; //
+const int flipperLeftPowerCoil = 9; // the number of the flipper pin
+
+const int slingshotOneSwitch = A6; // the number of the switch pin
+const int slingshotOne = 3; // the number of the slingshot pin
+//const int slingshotOneLED = 17; // the number of the slingshotLED pin
+
+const int slingshotTwoSwitch = A7; // the number of the switch pin
+const int slingshotTwo = 4; // the number of the slingshot pin
+//const int slingshotLeftLED = 18; // the number of the slingshotLED pin
+
+
+const int gatePin = 103;
+const int openTime = 1;
+
+const int coinDetect = 100;
+const int ballDeathSwith = 101;
+const int startButton = 102;
+
+const int newBalls = 5; //the number of new ball per pament
+const int minCoinsRequerd = 2; // minnum coins requard to get balls
+
+const int messageArraySize = 6;
+
+//points
+const int pointsForPopBumper = 10;
+const int pointsForSlingShot = 10;
+const int pointsForRollOver = 10;
+
+//free balls
+const int freeBalls = 1;
+const int freeBallPoints = 2000;
+
+//***************************************************************************************************************
+
+// PUBLIC VARs and setup
+
+int scoreRecord;
+int score;// the score for a inivial game
+int coinsCounted; // the total coin in the maching
+int coins; //
+int ballsRemaining;
+char message[messageArraySize];
+//char imputMessage;
+int freeBallPointsTracker = 0;
+
+
+bool gameState;
+
+
+//int money;
+
+void setup() {
+    // initialize the LED pin as an output:
+    pinMode(popBumperOne, OUTPUT);
+    pinMode(popBumperTwo, OUTPUT);
+    pinMode(popBumperThree, OUTPUT);
+
+    pinMode(flipperRightPowerCoil, OUTPUT);
+    pinMode(flipperRightHoldCoil, OUTPUT);
+
+    pinMode(flipperLeftPowerCoil, OUTPUT);
+    pinMode(flipperLeftHoldCoil, OUTPUT);
+
+    pinMode(slingshotOne, OUTPUT);
+    pinMode(slingshotTwo, OUTPUT);
+
+    // initialize the pushbutton pin as an input:
+    pinMode(popBumperOneSwitch, INPUT);
+    pinMode(popBumperTwoSwitch, INPUT);
+    pinMode(popBumperOneSwitch, INPUT);
+
+    pinMode(flipperRightHoldSwitch, INPUT);
+    pinMode(flipperLeftHoldSwitch, INPUT);
+    pinMode(flipperRightPowerSwitch, INPUT);
+    pinMode(flipperLeftPowerSwitch, INPUT);
+
+    pinMode(slingshotOneSwitch, INPUT);
+    pinMode(slingshotTwoSwitch, INPUT);
+
+    pinMode(coinDetect, INPUT);
+    pinMode(startButton, INPUT);
+    pinMode(ballDeathSwith, INPUT);
+
+    //initialize Serial comuticion
+    Serial.begin(9600); // start serial communication at 9600bps with USB port
+    Serial1.begin(9600); // start serial1 communication at 9600bps with screen control aduino
+
+    Serial.flush(); // flush communication
+    Serial1.flush(); // flush communication
+}
+
+
+void loop(){
+
+
+
+    ElectronicsLoop();
+
+    delay(5);
+}
+
+
+
+
+
+//******************************************************************************************************
+
+
+
+
+
+// ELECTRONICS CONTROL
+
+
+
+
+
+
+void ElectronicsLoop(){
+    SlingshotControl(slingshotOne, slingshotOneSwitch, pointsForSlingShot);
+    SlingshotControl(slingshotTwo, slingshotTwoSwitch, pointsForSlingShot);
+    PopBumperControl(popBumperOne, popBumperOneSwitch, pointsForPopBumper);
+    PopBumperControl(popBumperTwo, popBumperTwoSwitch, pointsForPopBumper);
+    PopBumperControl(popBumperThree, popBumperThreeSwitch, pointsForPopBumper);
+    FlipperControl(flipperLeftPowerCoil, flipperLeftHoldCoil, flipperLeftPowerSwitch, flipperLeftHoldSwitch);
+    FlipperControl(flipperRightPowerCoil, flipperRightHoldCoil, flipperRightPowerSwitch, flipperRightHoldSwitch);
+    TestSwitch(flipperLeftHoldSwitch, "flipperLeftHoldSwitch");
+    TestSwitch(flipperRightHoldSwitch, "flipperRightHoldSwitch");
+    TestSwitch(flipperLeftPowerSwitch, "flipperLeftPowerSwitch");
+    TestSwitch(flipperRightPowerSwitch, "flipperRightPowerSwitch");
+
+    
+}
+
+void BallGateControl (bool _open){ //puts ball into play NOT DONE
+    if (_open){
+        //open
+        //digitalWrite(gatePin, HIGH);
+        //Serial.println("RUNNING: gate open");
+    } else {
+        //Serial.println("RUNNING: gate closed");
+        //close
+    }
+}
+
+void PopBumperControl(int _popBumper, int _popBumperSwitch, int _points){ 
+
+    int buttonState = 1; // variable for reading the pushbutton status
+    // read the state of the pushbutton value:
+    buttonState = digitalRead(_popBumperSwitch);
+
+    // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+    if (buttonState == HIGH) {
+        // turn LED on:
+       
+        digitalWrite(_popBumper, HIGH);
+        delay(40);
+        digitalWrite(_popBumper, LOW);
+        Serial.print("RUNNING: PopBumperControl  ");
+    } else {
+        // turn LED off:
+        digitalWrite(_popBumper, LOW);
+    }
+}
+
+
+void FlipperControl(int _flipperPowerCoil, int _flipperHoldCoil, int _flipperPowerSwitch, int _flipperHoldSwitch) {
+
+    int buttonState = 1;
+    int button2State = 1;
+    // read the state of the pushbutton value:
+    buttonState = digitalRead(_flipperPowerSwitch);
+    button2State = digitalRead(_flipperHoldSwitch);
+
+    // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+    if (buttonState == HIGH && button2State == LOW) {
+        
+        digitalWrite(_flipperHoldCoil, HIGH);
+        digitalWrite(_flipperPowerCoil, HIGH);
+    } else if (buttonState == HIGH && button2State == HIGH){
+        digitalWrite(_flipperPowerCoil, LOW);
+    } else {
+        // turn LED off:
+        digitalWrite(_flipperPowerCoil, LOW);
+        digitalWrite(_flipperHoldCoil, LOW);
+    }
+}
+
+void TestSwitch (int _switch, String _name){
+  int switchState = digitalRead(_switch);
+  if (switchState == HIGH){
+    Serial.println(_name);
+  }
+}
+
+void SlingshotControl(int _slingshot, int _slingshotSwitch, int _points) {//NOT DONE
+
+    int buttonState = 1; // variable for reading the pushbutton status
+    // read the state of the pushbutton value:
+    buttonState = digitalRead(_slingshotSwitch);
+
+    // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+    if (buttonState == HIGH) {
+        // turn LED on:
+        Serial.print(_slingshotSwitch);
+        Serial.print(" ");
+        //AddScore(_points);
+        digitalWrite(_slingshot, HIGH);
+        delay(40);
+        digitalWrite(_slingshot, LOW);
+    } else {
+        // turn LED off:
+        digitalWrite(_slingshot, LOW);
+    }
+}
+
---
+++ trash/Pinball-master/Code/terren_tries_to_get_it_working/slave/slave/slave.ino
@@ -0,0 +1,154 @@
+//
+/*  
+INSTRUCTIONS:
+
+Using the serial monitor, type in the header "$PIN" followed by the ID you've chosen
+for the PinScore display (ie, 1, 2, 3, 4 etc), and then the actual score for that unit.
+
+EXAMPLE: Typing in "$PIN4100000" to the serial monitor and hitting return should read
+the score "100000"  player four's display.
+
+For questions or comments, check the blog:
+https://howtobuildapinballmachine.wordpress.com
+
+*/
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+unsigned char selectDigit = 0;  // start no digit selected
+const unsigned char selectLatch = 12; // start no digit selected
+unsigned char cDataPinScore[8];
+unsigned char cPinScore = 4;
+unsigned char digitBCD = 0; // start no digit selected
+
+// ASCII CODE DEFINES
+#define ASCII_A 65
+#define ASCII_B 66
+#define ASCII_C 67
+#define ASCII_D 68
+#define ASCII_E 69
+#define ASCII_F 70
+#define ASCII_G 71
+#define ASCII_H 72
+#define ASCII_I 73
+#define ASCII_J 74
+#define ASCII_K 75
+#define ASCII_L 76
+#define ASCII_M 77
+#define ASCII_N 78
+#define ASCII_O 79
+#define ASCII_P 80
+#define ASCII_Q 81
+#define ASCII_R 82
+#define ASCII_S 83
+#define ASCII_T 84
+#define ASCII_U 85
+#define ASCII_V 86
+#define ASCII_W 87
+#define ASCII_X 88
+#define ASCII_Y 89
+#define ASCII_Z 90
+#define ASCII_EQUAL 61
+#define ASCII_DOLLAR 36
+#define ASCII_COMMA 44
+#define ASCII_CR 13
+#define ASCII_LF 10
+
+enum  
+{
+  LED_ZERO,
+  LED_ONE,
+  LED_TWO,
+  LED_THREE,
+  PLAYER_ONE,
+  PLAYER_TWO,
+  CREDIT_BALLS  
+};
+
+#define LED_CHAN PLAYER_ONE // change this to the PLAYER that the unit will be driving
+
+void setup() 
+{
+  DDRB = B00111111;  // sets Arduino port B pins 0 to 4 as outputs
+  DDRD = B11111111;  // sets Arduino port B pins 0 to 4 as outputs
+
+  // initialize serial:
+  Serial.begin(9600);
+
+  // reserve 200 bytes for the inputString:
+  inputString.reserve(200);
+
+    for (int i = 0; i < 8; i++) 
+  {
+    cDataPinScore[i] = i + 48;
+  }
+
+}
+
+void loop() 
+{
+
+// continuously scan the PinScore display, outputing the most recent data
+  for (int i = 2; i < 8; i++) 
+  {
+    selectDigit = i;  
+
+    PORTD = (0); // clear all
+
+    digitalWrite(selectDigit,HIGH);
+
+    PORTB = (cDataPinScore[i-2]- 48); // subracting 48 converts an ASCII char to its equivalent int
+
+    digitalWrite(selectLatch,HIGH);
+
+    delay(3) ;// hold each digit for a period
+  }
+
+}
+
+/*
+  SerialEvent occurs whenever a new data comes in the
+ hardware serial RX.  This routine is run between each
+ time loop() runs, so using delay inside loop can delay
+ response.  Multiple bytes of data may be available.
+ */
+
+void serialEvent() 
+{
+  while (Serial.available()) 
+  {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+
+    // add it to the inputString:
+    inputString += inChar;
+
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n') 
+    {
+      int inputLength = inputString.length();
+
+      if (inputString[0] == ASCII_DOLLAR) // check for header
+      {
+        cPinScore = inputString[4]; // PinScore id held here
+
+        if (cPinScore == (LED_CHAN + 48)) //4 )
+        {
+          for (int k = 0; k<6; k++) // acount for numbers less than six digits
+      {
+      cDataPinScore[k] = inputString[k + 5];
+      } // next k
+
+        } // end If received ID matches the one this unit is programmed for
+
+        stringComplete = true;
+        inputString = "";
+
+      } // end If first char was $ 
+
+    } // end If new line 
+
+  } // end While
+
+} // end Sub
