62a3725514f77bf4c38dc1cd6849ce30d7581d7e
eli gottsaker <eligottsacker@gmail.com>
2018-05-14 00:41:47 -0700

    commit

---
+++ WithPi/piMain.cpp
@@ -0,0 +1,19 @@
+/* Libraries */
+#include <iostream>
+#include <pigpio.h>
+#include <string>
+#include <sstream>
+
+
+//pins
+
+
+//main
+class main {
+  while(true){
+
+  }
+  void GetArduino (){
+  }
+  void Transmit(string _data){}
+};
