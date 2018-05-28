b9d72f2d8758e9399aff216cfc32d10e3126578e
eli gottsaker <eligottsacker@gmail.com>
2018-05-28 13:45:37 -0700

    COMMIT

--- WithPi/helloWorld.cpp
+++ WithPi/helloWorld.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <stdio.h>
+#include <wiringPi.h>
 
 int main() {
+
+  wiringPiSetup () ;
+  pinMode (0, OUTPUT) ;
+  while (true)
+  {
+    digitalWrite (0, HIGH) ;
+    delay (500) ;
+    digitalWrite (0,  LOW) ;
+    delay (500) ;
+  }
 
   printf("hello world \r\n");
   return 0;
